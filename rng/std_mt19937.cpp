fefbbfbe5782372561aefc5be604b82b2e5e1a52
lyrahgames <markuspawellek@gmail.com>
2019-04-23 21:18:39 +0200

    Implement async pipeline for random bitstream

--- rng/std_mt19937.cpp
+++ rng/std_mt19937.cpp
@@ -1,13 +1,34 @@
-#include <cstdint>
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <future>
 #include <iostream>
 #include <random>
+#include <thread>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 int main() {
   mt19937 rng{random_device{}()};
+
+  // while (true) {
+  //   const uint32_t sample = rng();
+  //   cout.write(reinterpret_cast<const char*>(&sample), sizeof(sample));
+  // }
+
+  constexpr int c = 100'000;
+  vector<uint32_t> cache1(c);
+  auto cache2 = cache1;
+  generate(cache1.begin(), cache1.end(), ref(rng));
   while (true) {
-    const uint32_t sample = rng();
-    cout.write(reinterpret_cast<const char*>(&sample), sizeof(sample));
+    swap(cache1, cache2);
+    auto handle = async(launch::async, [&cache2]() {
+      cout.write(reinterpret_cast<const char*>(cache2.data()),
+                 sizeof(uint32_t) * cache2.size());
+    });
+    generate(cache1.begin(), cache1.end(), ref(rng));
+    handle.wait();
   }
 }
--- rng/std_random_device.cpp
+++ rng/std_random_device.cpp
@@ -1,36 +1,34 @@
+#include <algorithm>
 #include <chrono>
+#include <functional>
+#include <future>
 #include <iostream>
 #include <random>
+#include <thread>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
 int main() {
-  constexpr int n = 10'000'000;
-  constexpr int c = 10'000'000;
-
   random_device rng{};
 
-  const auto start = chrono::high_resolution_clock::now();
-  for (int i = 0; i < n; ++i) {
-    const auto sample = rng();
-    cout.write(reinterpret_cast<const char*>(&sample), sizeof(sample));
-  }
-  const auto end = chrono::high_resolution_clock::now();
+  // while (true) {
+  //   const auto sample = rng();
+  //   cout.write(reinterpret_cast<const char*>(&sample), sizeof(sample));
+  // }
 
-  const auto cache_start = chrono::high_resolution_clock::now();
-  vector<random_device::result_type> cache(c);
-  for (int i = 0; i < n; i += c) {
-    for (int j = 0; j < c; ++j) {
-      cache[j] = rng();
-    }
-    cout.write(reinterpret_cast<const char*>(cache.data()),
-               sizeof(random_device::result_type) * c);
+  constexpr int c = 1'000'000;
+  vector<random_device::result_type> cache1(c);
+  auto cache2 = cache1;
+  generate(cache1.begin(), cache1.end(), ref(rng));
+  while (true) {
+    swap(cache1, cache2);
+    auto handle = async(launch::async, [&cache2]() {
+      cout.write(reinterpret_cast<const char*>(cache2.data()),
+                 sizeof(random_device::result_type) * cache2.size());
+    });
+    generate(cache1.begin(), cache1.end(), ref(rng));
+    handle.wait();
   }
-  const auto cache_end = chrono::high_resolution_clock::now();
-
-  cout << "\n\nserial time = " << chrono::duration<float>(end - start).count()
-       << '\n'
-       << "cache time = "
-       << chrono::duration<float>(cache_end - cache_start).count() << '\n';
 }
