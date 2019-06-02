79d9efef610d97bc7588ea08695b2d7a8e21eeaa
lyrahgames <markuspawellek@gmail.com>
2019-06-02 18:51:52 +0200

    Try to tweak pipelined output for dieharder

--- rng/std_mt19937.cpp
+++ rng/std_mt19937.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <chrono>
+#include <cstdio>
 #include <functional>
 #include <future>
 #include <iostream>
@@ -13,20 +14,29 @@ using namespace std;
 int main() {
   mt19937 rng{random_device{}()};
 
+  // freopen(nullptr, "wb", stdout);
+  // while (1) {
+  //   uint32_t value = rng();
+  //   fwrite((void*)&value, sizeof(value), 1, stdout);
+  // }
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
   // while (true) {
   //   const uint32_t sample = rng();
   //   cout.write(reinterpret_cast<const char*>(&sample), sizeof(sample));
   // }
 
-  constexpr int c = 100'000;
-  vector<uint32_t> cache1(c);
-  auto cache2 = cache1;
+  cout.setf(ios::unitbuf);
+  constexpr int c = 1 << 20;
+  vector<uint32_t> cache1(c), cache2(c);
   generate(cache1.begin(), cache1.end(), ref(rng));
   while (true) {
     swap(cache1, cache2);
     auto handle = async(launch::async, [&cache2]() {
       cout.write(reinterpret_cast<const char*>(cache2.data()),
                  sizeof(uint32_t) * cache2.size());
+      // fwrite((void*)cache2.data(), sizeof(uint32_t), cache2.size(), stdout);
     });
     generate(cache1.begin(), cache1.end(), ref(rng));
     handle.wait();
