4725b2fa52818590b69b2686a70ec505aafe4dae
lyrahgames <markuspawellek@gmail.com>
2019-11-18 23:22:53 +0100

    Tweak API of scalar MT19937

--- pxart/mt19937.hpp
+++ pxart/mt19937.hpp
@@ -1,65 +1,95 @@
 #pragma once
-
+#include <cstddef>
 #include <cstdint>
+#include <pxart/algorithm.hpp>
 
 namespace pxart {
 
 struct mt19937 {
-  using int_type = uint64_t;
-  static constexpr int_type N = 624;
-  static constexpr int_type M = 397;
-  static constexpr int_type MATRIX_A = 0x9908b0dfUL;
-  static constexpr int_type UPPER_MASK = 0x80000000UL;
-  static constexpr int_type LOWER_MASK = 0x7fffffffUL;
-  static constexpr int_type mag01[2] = {0x0UL, MATRIX_A};
+  using uint_type = uint32_t;
+  using result_type = uint_type;
+  static constexpr size_t word_size = 32;
+  static constexpr size_t state_size = 624;
+  static constexpr size_t shift_size = 397;
+  static constexpr size_t mask_bits = 31;
+  static constexpr uint_type xor_mask = 0x9908b0dfu;
+  static constexpr uint_type tempering_b_mask = 0x9d2c5680u;
+  static constexpr uint_type tempering_c_mask = 0xefc60000u;
+  static constexpr size_t tempering_u_shift = 11;
+  static constexpr size_t tempering_s_shift = 7;
+  static constexpr size_t tempering_t_shift = 15;
+  static constexpr size_t tempering_l_shift = 18;
+  static constexpr uint_type default_seed = 5489u;
+  static constexpr uint_type init_multiplier = 1812433253u;
 
-  mt19937() = default;
+  static constexpr uint_type mask = (~uint_type{}) >>
+                                    (sizeof(uint_type) * 8 - word_size);
+  static constexpr uint_type upper_mask = ((~uint_type{}) << mask_bits) & mask;
+  static constexpr uint_type lower_mask = (~upper_mask) & mask;
+
+  struct default_seeder;
+
   template <typename RNG>
-  explicit mt19937(RNG& rng) {
-    for (auto i = N; i > 0; --i) state[N - i] = rng();
-  }
+  constexpr explicit mt19937(RNG&& rng);
+  constexpr mt19937();
+  mt19937(const mt19937&) = default;
+  mt19937& operator=(const mt19937&) = default;
+  mt19937(mt19937&&) = default;
+  mt19937& operator=(mt19937&&) = default;
 
-  constexpr void init(int_type s) noexcept {
-    state[0] = s & 0xffffffffUL;
-    for (int i = 1; i < N; ++i) {
-      state[i] = (1812433253UL * (state[i - 1] ^ (state[i - 1] >> 30)) + i);
-      state[i] &= 0xffffffffUL;
-    }
-  }
+  constexpr result_type operator()() noexcept;
+  constexpr result_type min() noexcept { return uint_type{}; }
+  constexpr result_type max() noexcept { return (~uint_type{}) & mask; }
 
-  constexpr uint32_t operator()() noexcept {
-    const auto generator = [](auto y) {
-      y ^= (y >> 11);
-      y ^= (y << 7) & 0x9d2c5680UL;
-      y ^= (y << 15) & 0xefc60000UL;
-      y ^= (y >> 18);
-      return y;
+  uint_type state[state_size];
+  int state_index = state_size;
+};
+
+struct mt19937::default_seeder {
+  constexpr explicit default_seeder(uint_type s = default_seed) : x{s & mask} {}
+  constexpr uint_type operator()() noexcept;
+  constexpr uint_type min() noexcept { return uint_type{}; }
+  constexpr uint_type max() noexcept { return (~uint_type{}) & mask; }
+  uint_type x;
+  uint_type c{0};
+};
+
+constexpr mt19937::uint_type mt19937::default_seeder::operator()() noexcept {
+  const auto result = x;
+  x = (x ^ (x >> (word_size - 2)));
+  x = (init_multiplier * x + (++c)) & mask;
+  return result;
+}
+
+template <typename RNG>
+constexpr mt19937::mt19937(RNG&& rng) {
+  generate(std::forward<RNG>(rng), state, state + state_size);
+}
+
+constexpr mt19937::mt19937() : mt19937{default_seeder{}} {}
+
+constexpr mt19937::result_type mt19937::operator()() noexcept {
+  if (state_index >= state_size) {
+    const auto transition = [this](int k, int k1, int k2) constexpr {
+      const auto x = (state[k] & upper_mask) | (state[k1] & lower_mask);
+      state[k] = state[k2] ^ (x >> 1) ^ ((x & 0x01) ? xor_mask : 0);
     };
 
-    if (state_index >= N) {
-      int_type y{};
-      // if (state_index == N + 1) init(5489);
-      for (int k = 0; k < N - M; ++k) {
-        y = (state[k] & UPPER_MASK) | (state[k + 1] & LOWER_MASK);
-        state[k] = state[k + M] ^ (y >> 1) ^ mag01[y & 0x1UL];
-      }
-      for (int k = N - M; k < N - 1; ++k) {
-        y = (state[k] & UPPER_MASK) | (state[k + 1] & LOWER_MASK);
-        state[k] = state[k + (M - N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
-      }
-      y = (state[N - 1] & UPPER_MASK) | (state[0] & LOWER_MASK);
-      state[N - 1] = state[M - 1] ^ (y >> 1) ^ mag01[y & 0x1UL];
-      state_index = 0;
-    }
+    for (int k = 0; k < state_size - shift_size; ++k)
+      transition(k, k + 1, k + shift_size);
+    for (int k = state_size - shift_size; k < state_size - 1; ++k)
+      transition(k, k + 1, k + shift_size - state_size);
+    transition(state_size - 1, 0, shift_size - 1);
 
-    return generator(state[state_index++]);
+    state_index = 0;
   }
 
-  constexpr int_type min() noexcept { return 0; }
-  constexpr int_type max() noexcept { return 0xffffffff; }
-
-  int_type state[N];
-  int state_index = N + 1;
-};
+  auto y = state[state_index++];
+  y ^= (y >> tempering_u_shift);
+  y ^= (y << tempering_s_shift) & tempering_b_mask;
+  y ^= (y << tempering_t_shift) & tempering_c_mask;
+  y ^= (y >> tempering_l_shift);
+  return y;
+}
 
 }  // namespace pxart
