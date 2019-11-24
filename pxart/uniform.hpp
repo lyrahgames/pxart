214f93d845f8443bd8a949b3c3b518fa45868e4a
lyrahgames <markuspawellek@gmail.com>
2019-11-24 07:33:36 +0100

    Fix 64bit output of std::mt19937 by specialization

--- pxart/uniform.hpp
+++ pxart/uniform.hpp
@@ -67,4 +67,16 @@ constexpr inline Real uniform(RNG&& rng, Real a, Real b) noexcept {
   return detail::uniform(std::forward<RNG>(rng)(), a, b);
 }
 
+template <typename Real>
+constexpr inline Real uniform(std::mt19937& rng, Real a = 0,
+                              Real b = 1) noexcept {
+  return detail::uniform(static_cast<uint32_t>(rng()), a, b);
+}
+
+template <typename Real>
+constexpr inline Real uniform(std::mt19937&& rng, Real a = 0,
+                              Real b = 1) noexcept {
+  return detail::uniform(static_cast<uint32_t>(std::move(rng)()), a, b);
+}
+
 }  // namespace pxart
