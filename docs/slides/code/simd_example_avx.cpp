782d37b5f59da883114e6db46952d0d747f168ab
lyrahgames <markuspawellek@gmail.com>
2020-05-22 19:02:25 +0200

    Tweak some slides

--- docs/slides/code/simd_example_avx.cpp
+++ docs/slides/code/simd_example_avx.cpp
@@ -1,5 +1,5 @@
 __m256d x = pxart::uniform<double>(vrng);
-__m256y y = pxart::uniform<double>(vrng);
+__m256d y = pxart::uniform<double>(vrng);
 
 __m256d x2 = _mm256_mul_pd(x, x);
 __m256d y2 = _mm256_mul_pd(y, y);
