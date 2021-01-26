d1bd3149d201e7840ae62ec0c1d6080735cfc991
lyrahgames <lyrahgames@mailbox.org>
2021-01-26 02:09:23 +0100

    Add uniform standard disk distribution

---
+++ pxart/distribution/disk.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cmath>
+//
+#include <pxart/distribution/uniform.hpp>
+#include <pxart/meta.hpp>
+
+namespace pxart {
+
+template <generic::floating_point R, generic::random_bit_generator G>
+inline auto disk(G&& g) {
+  while (true) {
+    const auto x = uniform<R>(g, -1, 1);
+    const auto y = uniform<R>(g, -1, 1);
+    if (x * x + y * y < R{1}) return std::pair{x, y};
+  }
+}
+
+}  // namespace pxart
