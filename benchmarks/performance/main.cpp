54d9d168f42acb41f175b6392c3204bd64ea041c
lyrahgames <lyrahgames@mailbox.org>
2021-02-03 16:47:04 +0100

    Tweak performance benchmark

--- benchmarks/performance/main.cpp
+++ benchmarks/performance/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <numeric>
 #include <random>
+#include <sstream>
 #include <vector>
 //
 #ifndef PXART_BENCHMARKS_DISABLE_BOOST
@@ -21,11 +22,7 @@
 
 using namespace std;
 
-const size_t n = 1'000'000;
-const size_t run_count = 100;
-
 random_device rd{};
-volatile uint64_t do_not_optimize_away;
 
 PXART_TEMPLATE(pxart::generic::random_bit_generator, G,
                (pxart::generic::seedable_by<G, random_device>))
@@ -42,98 +39,135 @@ inline auto seeded_generator<boost::random::mt19937>() {
 #endif  // PXART_BENCHMARKS_DISABLE_BOOST
 
 PXART_TEMPLATE(pxart::generic::random_bit_generator, G)
-double performance_runner() {
+static inline double benchmark_performance(const size_t samples) {
   auto g = seeded_generator<G>();
   pxart::meta::result<G> result = 0;
 
   // Warm-Up
-  for (size_t i = 0; i < n; ++i) result += g();
-
-  const auto samples = n;
-
-  const auto start = chrono::high_resolution_clock::now();
-
   for (size_t i = 0; i < samples; ++i) result += g();
 
+  // Measure
+  const auto start = chrono::high_resolution_clock::now();
+  {
+    for (size_t i = 0; i < samples; ++i) result += g();
+  }
   const auto end = chrono::high_resolution_clock::now();
   const auto time = chrono::duration<double>(end - start).count();
 
-  do_not_optimize_away = result;
-  // cout << result;
+  volatile auto do_not_optimize_away = result;
 
-  const auto performance = n * 4 / time / (1024.0 * 1024.0);
+  const auto performance = samples * 4 / time / (1024.0 * 1024.0);
   return performance;
 }
 
-struct runner {
-  const char* name;
-  double (*run)();
-  double max = 0;
-  double mean = 0;
-  double stddev = 0;
-};
-vector<runner> runners{};
+struct benchmark {
+  size_t n = 1'000'000;
+  size_t run_count = 100;
 
-int main() {
-  runners.push_back({"pxart::mt19937", performance_runner<pxart::mt19937>});
-  runners.push_back({"std::mt19937", performance_runner<std::mt19937>});
+  struct runner {
+    const char* name;
+    double (*run)(const size_t);
+    double max = 0;
+    double mean = 0;
+    double stddev = 0;
+  };
 
-#ifndef PXART_BENCHMARKS_DISABLE_BOOST
-  runners.push_back(
-      {"boost::random::mt19937", performance_runner<boost::random::mt19937>});
-#endif
+  vector<runner> runners{};
 
-  runners.push_back(
-      {"pxart::minstd_rand", performance_runner<pxart::minstd_rand>});
-  runners.push_back({"std::minstd_rand", performance_runner<std::minstd_rand>});
+  benchmark& add(const char* name, double (*run)(const size_t)) {
+    runners.push_back({name, run});
+    return *this;
+  }
+#define ADD(X) add(#X, benchmark_performance<X>)
 
-  vector<size_t> indices(runners.size());
-  iota(begin(indices), end(indices), 0);
+  benchmark& run() {
+    vector<size_t> indices(runners.size());
+    iota(begin(indices), end(indices), 0);
 
-  const auto samples = run_count;
-
-  for (size_t k = 0; k < samples; ++k) {
-    shuffle(begin(indices), end(indices), rd);
-    for (auto i : indices) {
-      const auto performance = runners[i].run();
-      runners[i].mean += performance;
-      runners[i].stddev += performance * performance;
-      runners[i].max = max(runners[i].max, performance);
-      // cout << setw(30) << left << runners[i].name << setw(25) << right
-      //      << performance << " MiB/s\n"
-      //      << flush;
+    for (size_t k = 0; k < run_count; ++k) {
+      shuffle(begin(indices), end(indices), rd);
+      // #pragma omp parallel for
+      for (auto i : indices) {
+        // for (size_t p = 0; p < indices.size(); ++p) {
+        //   const auto i = indices[p];
+        const auto performance = runners[i].run(n);
+        runners[i].mean += performance;
+        runners[i].stddev += performance * performance;
+        runners[i].max = max(runners[i].max, performance);
+        // cout << setw(30) << left << runners[i].name << setw(25) << right
+        //      << performance << " MiB/s\n"
+        //      << flush;
+      }
+      // cout << '\n';
     }
     // cout << '\n';
-  }
-  // cout << '\n';
 
-  for (size_t i = 0; i < runners.size(); ++i) {
-    runners[i].mean /= run_count;
-    runners[i].stddev = sqrt(
-        (runners[i].stddev - run_count * runners[i].mean * runners[i].mean) /
-        (run_count - 1));
+    for (size_t i = 0; i < runners.size(); ++i) {
+      runners[i].mean /= run_count;
+      runners[i].stddev = sqrt(
+          (runners[i].stddev - run_count * runners[i].mean * runners[i].mean) /
+          (run_count - 1));
+    }
+    return *this;
   }
 
-  for (size_t i = 0; i < runners.size(); ++i) {
-    cout << setw(30) << left << runners[i].name << right << setw(25)
-         << runners[i].max << " MiB/s" << setw(25) << runners[i].mean
-         << " MiB/s" << setw(25) << runners[i].stddev << " MiB/s" << '\n';
+  friend ostream& operator<<(ostream& os, benchmark& d) {
+    os << setw(30) << "Performance Benchmark" << setw(30)
+       << "samples/run = " << d.n << ", runs = " << d.run_count << '\n'
+       << string(105, '-') << '\n'
+       << left << setw(30) << "Name" << right << setw(25) << "Max. [MiB/s]"
+       << setw(25) << "Mean [MiB/s]" << setw(25) << "Stddev. [MiB/s]\n"
+       << string(105, '-') << '\n';
+    for (size_t i = 0; i < d.runners.size(); ++i) {
+      os << setw(30) << left << d.runners[i].name << right << setw(25)
+         << d.runners[i].max << setw(25) << d.runners[i].mean << setw(25)
+         << d.runners[i].stddev << '\n';
+    }
+    os << string(105, '-') << '\n';
+    return os;
   }
+};
+
+int main(int argc, char** argv) {
+  benchmark driver{};
+
+  if (argc == 3) {
+    stringstream input{argv[1]};
+    input >> driver.n;
+    input = stringstream{argv[2]};
+    input >> driver.run_count;
+  }
+
+  driver  //
+      .ADD(pxart::mt19937)
+      .ADD(std::mt19937)
+#ifndef PXART_BENCHMARKS_DISABLE_BOOST
+      .ADD(boost::random::mt19937)
+#endif
+      .ADD(pxart::minstd_rand)
+      .ADD(std::minstd_rand)
+      .run();
+  cout << driver;
 
 #ifndef PXART_BENCHMARKS_DISABLE_GNUPLOT
   fstream file{"performance.txt", ios::out};
-  for (size_t i = 0; i < runners.size(); ++i)
-    file << '"' << runners[i].name << '"' << '\t' << runners[i].max << '\t'
-         << runners[i].mean << '\t' << runners[i].stddev << '\n';
+  for (size_t i = 0; i < driver.runners.size(); ++i)
+    file << '"' << driver.runners[i].name << '"' << '\t'
+         << driver.runners[i].max << '\t' << driver.runners[i].mean << '\t'
+         << driver.runners[i].stddev << '\n';
   file << flush;
   lyrahgames::gnuplot_pipe plot{};
-  plot << "unset key\n"
-          "set xr [-1:5]\n"
+  plot << "set xr [-1:" << driver.runners.size() << "]\n"
+       << "set title \"Performance Benchmark (samples/run = " << driver.n
+       << ", runs = " << driver.run_count << ")\"\n"
+       << "unset key\n"
           "set yr [0:1000 < *]\n"
           "set yl \"Performance [MiB/s]\"\n"
           "set xtics rotate by 20 right\n"
           "set xtics nomirror scale 0\n"
           "plot 'performance.txt' u 0:2:(0.7):xtic(1) w boxes lc rgb '#222222' "
-          "fs solid 0.2\n";
+          "fs solid 0.2, "
+          "'' u 0:3:(0.7):xtic(1) w boxes lc rgb '#222222', "
+          "'' u 0:3:4 w yerrorbars lw 2 lc rgb '#222222'\n";
 #endif  // PXART_BENCHMARKS_DISABLE_GNUPLOT
 }
